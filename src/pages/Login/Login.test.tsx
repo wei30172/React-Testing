b49fc58282b3a4c0ebadc9c7c917c59cdab1a91b
claireWei <claire610323@gmail.com>
2022-08-02 12:02:15 -0700

    Testing login page

---
+++ src/pages/Login/Login.test.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./Login";
+
+test("email and passwordinput should be rendered and emtpy", () => {
+  render(
+    <Router>
+      <Login />
+    </Router>,
+  );
+  const emailInputEl = screen.getByPlaceholderText(/Email/i) as HTMLInputElement;
+  const passwordInputEl = screen.getByPlaceholderText(/Password/i) as HTMLInputElement;
+  expect(emailInputEl).toBeInTheDocument();
+  expect(emailInputEl.value).toBe("");
+  expect(passwordInputEl).toBeInTheDocument();
+  expect(passwordInputEl.value).toBe("");
+});
+
+test("login button should be render and disabled", () => {
+  render(
+    <Router>
+      <Login />
+    </Router>,
+  );
+  const buttonEl = screen.getByRole("button");
+  expect(buttonEl).toBeInTheDocument();
+  expect(buttonEl).toBeDisabled();
+});
+
+test("email and password input should change", () => {
+  render(
+    <Router>
+      <Login />
+    </Router>,
+  );
+  const emailInputEl = screen.getByPlaceholderText(
+    /Email/i,
+  ) as HTMLInputElement;
+
+  const passwordInputEl = screen.getByPlaceholderText(
+    /Password/i,
+  ) as HTMLInputElement;
+
+  const testValue = "test";
+
+  fireEvent.change(emailInputEl, { target: { value: testValue } });
+  expect(emailInputEl.value).toBe(testValue);
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  expect(passwordInputEl.value).toBe(testValue);
+});
+
+test("button should not be disabled when inputs exist", () => {
+  render(
+    <Router>
+      <Login />
+    </Router>,
+  );
+  const buttonEl = screen.getByRole("button");
+  const emailInputEl = screen.getByPlaceholderText(
+    /Email/i,
+  ) as HTMLInputElement;
+  const passwordInputEl = screen.getByPlaceholderText(
+    /Password/i,
+  ) as HTMLInputElement;
+
+  const testValue = "test";
+
+  fireEvent.change(emailInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  expect(buttonEl).not.toBeDisabled();
+});
+
+test("loading should be rendered correctly. navigate to home after login successfully.", async () => {
+  render(
+    <Router>
+      <Login />
+    </Router>,
+  );
+  const buttonEl = screen.getByRole("button");
+  const emailInputEl = screen.getByPlaceholderText(
+    /Email/i,
+  ) as HTMLInputElement;
+  const passwordInputEl = screen.getByPlaceholderText(
+    /Password/i,
+  ) as HTMLInputElement;
+
+  const testValue = "test";
+
+  fireEvent.change(emailInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+  expect(buttonEl).toHaveTextContent(/please wait/i);
+  await waitFor(() => expect(buttonEl).not.toHaveTextContent(/please wait/i));
+  expect(window.location.pathname).toBe("/");
+});
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { InputWrapper } from "../../components";
+import axios from "axios";
+import { InputWrapper, ButtonWrapper } from "../../components";
 import ErrorIcon from "@material-ui/icons/Error";
+import { toast } from "react-hot-toast";
 import "./Login.scss";
 
 export interface LoginValues {
@@ -11,6 +13,7 @@ export interface LoginValues {
 
 const Login = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [userInputs, setUserInputs] = useState<LoginValues>({
     email: "",
     password: "",
@@ -22,7 +25,7 @@ const Login = () => {
       label: "Email",
       errorMessage: "Email should be a valid email address!",
       name: "email",
-      type: "email",
+      type: "text",
       placeholder: "Email",
       pattern: "^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$",
       required: true,
@@ -42,8 +45,19 @@ const Login = () => {
     },
   ];
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/1",
+      );
+      toast.success(`Welcome, ${data.name}!`);
+    } catch (err) {
+      toast.error("Login Failure.");
+    }
+    setLoading(false);
+    navigate("/");
   };
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
@@ -53,47 +67,37 @@ const Login = () => {
 
   return (
     <div className="login">
-    <div className="login_form">
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        {formInputs.map(
-          ({ id, name, label, errorMessage, ...inputProps }) => (
-            <div key={id} className="form-input">
-              <label>{label}</label>
-              <br />
-              <InputWrapper
-                {...inputProps}
-                name={name}
-                value={userInputs[name as keyof LoginValues]}
-                handleChange={handleChange}
-              />
-              <span>
-                <ErrorIcon />
-                {errorMessage}
-              </span>
-            </div>
-          ),
-        )}
-        <button
-          disabled={!userInputs.email || !userInputs.password}
-          className="btn"
-          type="submit"
-        >
-          LOGIN
-        </button>
-      </form>
-      <div className="login_form_link flex">
-        <p>Do not have an account?</p>
-        <button
-          className="cursor-pointer"
-          onClick={() => navigate("/signup")}
-        >
-          Signup
-        </button>
+      <div className="login_form">
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          {formInputs.map(
+            ({ id, name, label, errorMessage, ...inputProps }) => (
+              <div key={id} className="form-input">
+                <label>{label}</label>
+                <br />
+                <InputWrapper
+                  {...inputProps}
+                  name={name}
+                  value={userInputs[name as keyof LoginValues]}
+                  handleChange={handleChange}
+                />
+                <p data-testid={`error-${name}`}>
+                  <ErrorIcon />
+                  {errorMessage}
+                </p>
+              </div>
+            ),
+          )}
+          <ButtonWrapper
+            disabled={!userInputs.email || !userInputs.password}
+            className="btn"
+            type="submit"
+            title={loading ? "please wait" : "Login"}
+          />
+        </form>
       </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default Login;
