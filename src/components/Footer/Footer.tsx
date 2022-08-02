3bbad20e208d36252d0b151fa6e526b117fedc7f
claireWei <claire610323@gmail.com>
2022-08-01 17:21:30 -0700

    init

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.scss";
 function Footer() {
   return (
     <div className="footer">
-      <p>Copyright &copy; 2022 Company. All rights reserved.</p>
+      <p>Copyright &copy; 2022 React. All rights reserved.</p>
     </div>
   );
 }
